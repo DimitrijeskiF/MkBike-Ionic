0802d85624584a95a4770d727871f7d48fcaccd7
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2021-05-11 20:10:48 +0200

    saving fcm token to DB

--- src/app/services/messaging.service.ts
+++ src/app/services/messaging.service.ts
@@ -1,20 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AngularFireMessaging } from '@angular/fire/messaging';
 import { tap } from 'rxjs/operators'
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagingService {
   token = null;
+  BACKEND_URL = environment.uri;
 
-  constructor(private afMessaging: AngularFireMessaging) { }
+  constructor(
+    private afMessaging: AngularFireMessaging,
+    private http: HttpClient
+  ) { }
 
   requestPermission() {
     return this.afMessaging.requestToken.pipe(
       tap(token => {
         console.log('Store token to server: ', token);
+        this.createToken(token)
       })
     );
+  }
+
+
+  createToken(fcmToken: string) {
+    const token = {
+      fcmToken: fcmToken
+    }
+    this.http.post(this.BACKEND_URL + '/fcmTokens', token).subscribe()
   }
 }
